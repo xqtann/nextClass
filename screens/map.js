3f774596a96bba91da26f3527f6a5fb79735273d
xqtann <xqxinquan@gmail.com>
2024-06-04 22:21:14 +0800

    added venues

--- screens/map.js
+++ screens/map.js
@@ -14,6 +14,7 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import RadioGroup from 'react-native-radio-buttons-group';
 
 
+
 export default function Map({ navigation, route }) {
   const { destVenue } = route.params;
   const [location, setLocation] = useState(null);
